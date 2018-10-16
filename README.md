b8578e7341f010b90473199609300acc2da548a5
Phil Bain <bain.pmcg@gmail.com>
2018-10-16 23:06:28 +0200

    update package for maker

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# sdk-governance-services
-SDK Services for Governance Dashboard
-
-First pass at separating the SDK services from the Governance Dashboard project.
-
-* Checkout branch `sdk-integration-services` from the governance dashboard project.
-* Symlink this directory to the root of governance dashboard project
+# dai-plugin-governance
--- scripts/build-backend.sh
+++ scripts/build-backend.sh
@@ -1,24 +1,5 @@
 # #!/usr/bin/env bash
 
-# set -e
-
-# if [ "$1" = "dirty" ]; then
-#   echo "Dirty mode: not removing previous build files."
-# else
-#   rm -rf dist
-# fi
-
-# babel src --out-dir ./dist/src
-# cp package.json dist
-# mkdir -p dist/contracts/abis
-# cp -R contracts/abis/ dist/contracts/abis
-# cp -R contracts/addresses/ dist/contracts/addresses
-# cp src/contract-info.json dist/src/contract-info.json
-# cp src/testnet-addresses.json dist/src/testnet-addresses.json
-# babel contracts --out-dir ./dist/contracts
-
-###
-
 set -e
 
 if [ "$1" = "dirty" ]; then
@@ -28,7 +9,6 @@ else
 fi
 
 babel -q contracts --out-dir ./dist/contracts
-#babel -q contracts/addresses --out-dir ./dist/contracts/addresses
 babel -q src --out-dir ./dist/src
 
 copyfiles \
